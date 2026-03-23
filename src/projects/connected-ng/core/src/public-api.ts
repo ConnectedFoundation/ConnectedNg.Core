6dd62ace7967817981e3da2334a871c0c69473c3
Matija Koželj <matija.kozelj@tompit.com>
2026-03-23 17:12:02 +0100

    feat: add local storage service

--- src/projects/connected-ng/core/src/public-api.ts
+++ src/projects/connected-ng/core/src/public-api.ts
@@ -19,3 +19,4 @@ export * from './services/common/dtos/dto-descriptor';
 export * from './services/common/dtos/primary-key-dto';
 export * from './services/common/dtos/query-dto';
 export * from './services/common/dtos/primary-key-list-dto';
+export * from './services/common/local-storage-service';
