296dcd683c8b0ccbde4adb7522ae3997d9796632
mihaelocvirk <ocvirkmihael@gmail.com>
2026-03-26 15:20:38 +0100

    feat: add hasToken observable

--- src/projects/connected-ng/core/authentication/src/credential-storage-service.ts
+++ src/projects/connected-ng/core/authentication/src/credential-storage-service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CredentialStorageService {
   private readonly tokenKey = 'access_token';
+  _hasToken$ = new BehaviorSubject<boolean>(this.hasToken());
 
   getToken(): string | null {
     return localStorage.getItem(this.tokenKey);
@@ -12,13 +15,17 @@ export class CredentialStorageService {
 
   setToken(token: string): void {
     localStorage.setItem(this.tokenKey, token);
+    this._hasToken$.next(true);
   }
 
   clearToken(): void {
     localStorage.removeItem(this.tokenKey);
+    this._hasToken$.next(false);
   }
 
   hasToken(): boolean {
     return !!this.getToken();
   }
+
+  hasToken$ : Observable<boolean>  = this._hasToken$.asObservable();
 }
