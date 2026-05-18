12af64354aa7be7feb0cbc0a7b625517a9607514
Matija Koželj <matija.kozelj@tompit.com>
2026-05-18 12:38:49 +0200

    feat: move Connected user interface to proper library

--- src/projects/connected-ng/core/permissions/src/components/role-code-list/role-item-header/role-item-header.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'cn-role-item-header',
-  imports: [],
-  templateUrl: './role-item-header.html',
-  styleUrl: './role-item-header.scss',
-})
-export class RoleItemHeader {}
